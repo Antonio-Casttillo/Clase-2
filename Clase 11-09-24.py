97a2a81336b9b6ddea2d590d7289c4acd0ce2100
Antonio-Casttillo <antonio.casttillo626@gmail.com>
2024-09-11 21:59:33 -0600

    Add files via upload

---
+++ Clase 11-09-24.py
@@ -0,0 +1,23 @@
+#print(10 + 5)
+
+#print(120 % 50)
+a = 7
+b = 2
+c = 6
+x = 5
+y = 4 
+z = 3
+
+z += 1
+y *= 5
+x **= 2
+
+"""
+
+#Comentar
+print(not(x > z) or x < y and y < x)"""
+
+print("not(x > z or not(x < y and z > x) or x > y and z < y)")
+R = not(x > z or not(x < y and z > x) or x > y and z < y)
+print("El resultado es:" (R) )
+
